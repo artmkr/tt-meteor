eb4dedcbb6f5db92dec571b4673ee68a163ac662
nuenvio <nuenvio@gmail.com>
2016-04-11 21:36:58 +0300

    initialized signin and signup pages

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,10 +2,26 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 import '/imports/ui/pages/index/index.js'
+import '/imports/ui/pages/signin/signin.js'
+import '/imports/ui/pages/signup/signup.js'
 
 FlowRouter.route('/', {
   name: 'index',
   action() {
     BlazeLayout.render('index');
+  },
+});
+
+FlowRouter.route('/signin', {
+  name: 'signin',
+  action() {
+    BlazeLayout.render('signin');
+  },
+});
+
+FlowRouter.route('/signup', {
+  name: 'signup',
+  action() {
+    BlazeLayout.render('signup');
   },
 });
