b95c77db90b9d86941406717abf3738c869bfa74
nuenvio <nuenvio@gmail.com>
2016-04-12 20:39:23 +0300

    created signup page

--- imports/ui/layouts/main/main.js
+++ imports/ui/layouts/main/main.js
@@ -1,2 +1,3 @@
 import './main.html'
 import '../../components/header/header.html'
+import '../../components/footer/footer.html'
--- imports/ui/pages/signup/signup.js
+++ imports/ui/pages/signup/signup.js
@@ -1,1 +1,12 @@
 import './signup.html'
+
+Template.signup.events({
+  'submit form': function(event){
+    event.preventDefault();
+    var email = $('[name=email]').val();
+    var name  = $('[name=name]').val();
+    var password = $('[name=password]').val();
+    var confirmPassword = $('[name=password]').val();
+    console.log(name,email,password,confirmPassword);
+  }
+});
