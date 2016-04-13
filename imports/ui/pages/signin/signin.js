d5864b684be075b3affba17cca213173668d36b2
nuenvio <nuenvio@gmail.com>
2016-04-13 14:50:57 +0300

    signing logic and error handling

--- imports/ui/pages/signin/signin.js
+++ imports/ui/pages/signin/signin.js
@@ -1,1 +1,49 @@
 import './signin.html'
+import { Router } from 'meteor/iron:router';
+import { Session } from 'meteor/session';
+
+var ERRORS_KEY = 'joinErrors';
+
+Template.signin.onCreated(function () {
+  Session.set(ERRORS_KEY, {});
+});
+
+Template.signin.helpers({
+  errorMessages: function () {
+    return _.values(Session.get(ERRORS_KEY));
+  }
+});
+
+Template.signin.events({
+  'submit form': function (event) {
+    event.preventDefault();
+
+    var email = $('[name=email]').val();
+    var password = $('[name=password]').val();
+
+    var errors = {};
+
+    if (!email) {
+      errors.email = 'Email is required';
+    }
+
+    if (!password) {
+      errors.password = 'Password is required';
+    }
+
+    Session.set(ERRORS_KEY, errors);
+
+    if (_.keys(errors).length) {
+      console.log(_.keys(errors).length);
+      return;
+    }
+
+    Meteor.loginWithPassword(email, password, function (error) {
+      if (error) {
+        return Session.set(ERRORS_KEY, {'none': error.reason});
+      } else {
+        Router.go('index');
+      }
+    });
+  }
+});
--- imports/ui/pages/signup/signup.js
+++ imports/ui/pages/signup/signup.js
@@ -1,6 +1,20 @@
 import './signup.html'
 import { Router } from 'meteor/iron:router';
 
+import { Session } from 'meteor/session';
+
+var ERRORS_KEY = 'joinErrors';
+
+Template.signin.onCreated(function () {
+  Session.set(ERRORS_KEY, {});
+});
+
+Template.signin.helpers({
+  errorMessages: function () {
+    errors = Session.get(ERRORS_KEY);
+    return Object.keys(errors).map(key => errors[key]);
+  }
+});
 
 Template.signup.events({
   'submit form': function(event){
@@ -9,6 +23,28 @@ Template.signup.events({
     var name  = $('[name=name]').val();
     var password = $('[name=password]').val();
     var confirmPassword = $('[name=password-confirmation]').val();
+
+    if (!email) {
+      errors.email = 'Email is required';
+    }
+
+    if (!password) {
+      errors.password = 'Password is required';
+    }
+
+    if (!name) {
+      errors.password = 'Username is required';
+    }
+
+    if (password != confirmPassword) {
+      errors.password = 'Please confirm your password';
+    }
+
+    Session.set(ERRORS_KEY, errors);
+
+    if (_.keys(errors).length) {
+      return;
+    }
 
     var user = {
       name:name,
